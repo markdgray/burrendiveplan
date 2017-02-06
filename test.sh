2e37a71d523d3c9d6db82492ded283bdc8ef50b5
Mark Gray <mrmarkgray@gmail.com>
2017-02-06 21:04:42 +0000

    Add command line argument to select dive times
    Signed-off-by: Mark Gray <mrmarkgray@gmail.com>

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-./diveplan -d data.csv -s 01/02/17 -e 28/02/17 > results && diff results gold
+./diveplan -d data.csv -s 01/02/17 -e 28/02/17 -t "Sun 09:00" -t "Sun 12:00" -t "Wed 19:00" > results && diff results gold
 
